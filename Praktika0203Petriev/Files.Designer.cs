5a93d3c016294b7adf17e31364037d3c1b3d716f
Анна Саражина <btpit.upk@outlook.com>
2019-10-23 14:34:50 +0300

    Test(Final V)

--- Praktika0203Petriev/Files.Designer.cs
+++ Praktika0203Petriev/Files.Designer.cs
@@ -33,6 +33,7 @@
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -87,18 +88,28 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
             // Files
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(583, 154);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label1);
             this.Name = "Files";
-            this.Text = "Files";
+            this.Text = "Test";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,5 +122,6 @@
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Praktika0203Petriev/Files.cs
+++ Praktika0203Petriev/Files.cs
@@ -15,10 +15,17 @@ namespace Praktika0203Petriev
     {
         StreamReader streamReader = new StreamReader(@"C:\Users\k120\Desktop\Praktika0203Petriev\test\Questions.txt");
         int countClicks = 0;
+        RadioButton[] radioButtons = new RadioButton[3];
+        int[] trueAns = new int[] { 3, 1, 0, 2, 0, 1};
+        int countTrueAnsw = 0;
+
         public Files()
         {
             InitializeComponent();
-            
+            radioButtons[0] = radioButton1;
+            radioButtons[1] = radioButton2;
+            radioButtons[2] = radioButton3;
+            label2.Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -45,11 +52,24 @@ namespace Praktika0203Petriev
 
                 }
             }
+
+            try
+            {
+                if (radioButtons[trueAns[countClicks]].Checked == true)
+                {
+                    countTrueAnsw++;
+                }
+            }
+            catch
+            {
+
+            }
             
 
             switch (countClicks)
             {
-                case 1:
+                case 0:
+                    label2.Visible = false;
                     button1.Text = "Ответить";
                     break;
                 case 4:
@@ -58,18 +78,23 @@ namespace Praktika0203Petriev
                     streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
                     break;
                 case 5:
-                    button1.Text = "Начать";
+                    label2.Visible = true;
+                    label2.Text = "Тест завершён! Количество правильных ответов: " + countTrueAnsw.ToString();
+                    button1.Text = "Начать заново";
                     label1.Text = "Вопрос";
                     radioButton1.Text = "а)";
                     radioButton2.Text = "б)";
                     radioButton3.Text = "в)";
+
                     break;
             }
-            if (countClicks == 5)
+            
+            countClicks++;
+            if (countClicks == 6)
             {
                 countClicks = 0;
+                countTrueAnsw = 0;
             }
-            countClicks++;
 
         }
     }
